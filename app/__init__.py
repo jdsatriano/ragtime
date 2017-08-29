7a0b77e7c9450908599235d2a1df3a0e6478c13d
Josh Satriano <jdsatch@gmail.com>
2017-08-29 10:18:48 -0500

    change Procfile

--- app/__init__.py
+++ app/__init__.py
@@ -15,7 +15,7 @@ loginManager.init_app(app)
 #MySQL configurations
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Satchabooty30'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'b69d91c6'
 app.config['MYSQL_DATABASE_DB'] = 'ragtime'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
